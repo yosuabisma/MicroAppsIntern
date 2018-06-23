dc07353be2318ba05697b3acd9f2d0375467bea2
yosuabisma <yosuabisma@gmail.com>
2018-06-24 02:29:28 +0700

    syalala

---
+++ tebak-pola-angka/src/components/PlayPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="play-page">
+    <div v-if="quest1===true" class="question-1">
+      {{num1}} , {{num2}}
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        quest1:true,
+        quest2:false,
+        quest3:false,
+        quest4:false,
+        quest5:false,
+        quest6:false,
+        quest7:false,
+        quest8:false,
+        quest9:false,
+        quest10:false,
+        num1:Math.floor(Math.random() * Math.floor(100)),
+        num2:
+      }
+    },
+    methods: {
+      gotoPage(route) {
+        this.$router.push(route);
+      },
+      generateQuest(){
+        this.num1=Math.floor(Math.random() * Math.floor(100));
+      }
+    }
+  }
+</script>
+<style>
+</style>
